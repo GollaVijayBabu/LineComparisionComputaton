912256789187d31dc98532df5ac7e71a2a79c26d
GollaVijayBabu <gollavijaybabu221@gmail.com>
2021-10-27 01:15:38 -0700

    Line Comparision Computation

--- Program.cs
+++ Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 
-namespace LineComparisionComputaton
+namespace ComparisonComputation
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To Line Comparision Computation"); 
+            Console.WriteLine("Welcome to Comparison Computation Program");
+
+            UC_1 length = new UC_1();
+            length.CalculateLengthOfLine();
+
+            UC_2 equal = new UC_2();
+            equal.EqualityofLines();
+
+            UC_3 comparison = new UC_3();
+            comparison.ComparisonOfLines();
+
+            UC_4 uc = new UC_4();
+            uc.ComparisonOfLines();
         }
     }
 }
+
